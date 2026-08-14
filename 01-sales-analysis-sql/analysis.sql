cba362b25a25bae10baffdbdaa92cb80c76ee735
gift-knowledge <Kip.natkan2@gmail.com>
2026-08-14 19:03:33 +0700

    Fix Q1 clean code and remove inline comments

--- 01-sales-analysis-sql/analysis.sql
+++ 01-sales-analysis-sql/analysis.sql
@@ -118,30 +118,3 @@ LIMIT 1; -- Retrieved only the bottom customer with lowest revenue
 
 
 
-SELECT 
-    product,
-    price,
-    CASE
-        WHEN price >= 5000 THEN 'High'
-        ELSE 'Normal'
-    END AS price_level
-FROM orders;
-
-
-SELECT 
-    customer,
-    SUM(quantity * price) AS total_revenue
-FROM orders
-GROUP BY customer
-HAVING total_revenue > 5000;
-
-
-SELECT 
-    customer,
-    SUM(quantity * price) AS total_revenue
-FROM orders
-GROUP BY customer
-HAVING total_revenue > 5000
-ORDER BY total_revenue DESC;
-
-
