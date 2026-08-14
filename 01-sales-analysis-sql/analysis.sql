dd3609213e5e372f1225c448a7b182335fd50bd3
gift-knowledge <Kip.natkan2@gmail.com>
2026-08-14 19:17:21 +0700

    Fix Q1 clean code and remove inline comments

--- 01-sales-analysis-sql/analysis.sql
+++ 01-sales-analysis-sql/analysis.sql
@@ -1,5 +1,17 @@
-CREATE TABLE orders(order_id INTEGER PRIMARY KEY, customer TEXT, product TEXT, category TEXT, quantity INTEGER, price INTEGER); INSERT INTO orders VALUES (1, 'Alice','Laptop', 'Electronics', 1, 25000), (2, 'Bob', 'Mouse', 'Electronics', 2, 500), (3, 'Alice', 'Keyboard', 'Electronics', 1, 1500), (4, 'John', 'Chair', 'Furniture', 2, 3000), (5, 'Bob', 'Desk', 'Furniture', 1, 5000);
-SELECT COUNT (order_id) AS total_orders FROM orders; SELECT SUM(quantity * price) AS total_revenue FROM orders; SELECT customer, SUM(quantity * price) AS customer_revenue FROM orders GROUP BY customer; SELECT category, SUM(quantity * price) AS category_revenue FROM orders GROUP BY category; SELECT product, SUM(quantity * price) AS product_revenue FROM orders GROUP BY product ORDER BY product_revenue DESC LIMIT 1; 
+CREATE TABLE orders(order_id INTEGER PRIMARY KEY, customer TEXT, product TEXT, category TEXT, quantity INTEGER, price INTEGER); 
+INSERT INTO orders VALUES (1, 'Alice','Laptop', 'Electronics', 1, 25000), 
+(2, 'Bob', 'Mouse', 'Electronics', 2, 500), 
+(3, 'Alice', 'Keyboard', 'Electronics', 1, 1500), 
+(4, 'John', 'Chair', 'Furniture', 2, 3000), 
+(5, 'Bob', 'Desk', 'Furniture', 1, 5000);
+
+
+
+SELECT COUNT (order_id) AS total_orders FROM orders; 
+SELECT SUM(quantity * price) AS total_revenue FROM orders; 
+SELECT customer, SUM(quantity * price) AS customer_revenue FROM orders GROUP BY customer; 
+SELECT category, SUM(quantity * price) AS category_revenue FROM orders GROUP BY category; 
+SELECT product, SUM(quantity * price) AS product_revenue FROM orders GROUP BY product ORDER BY product_revenue DESC LIMIT 1; 
 
 
 
